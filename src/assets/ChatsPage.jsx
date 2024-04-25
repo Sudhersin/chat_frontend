5cbb20e4b825d88d277e5596216286c1f1c9b0ef
sudhersini <sudhersini@gmail.com>
2024-04-25 18:55:55 +0530

    frontend commit

---
+++ src/assets/ChatsPage.jsx
@@ -0,0 +1,20 @@
+//import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
+import {PrettyChatWindow} from 'react-chat-engine-pretty'
+const ChatsPage =(props) => {
+    // const chatProps = useMultiChatLogic(
+    //     '0ca55d5d-c412-40eb-881b-2b98c5f84025',
+    //     props.user.username,
+    //     props.user.secret
+    return (
+        <div style={{height:'100vh'}}>
+            <PrettyChatWindow
+               projectId='0ca55d5d-c412-40eb-881b-2b98c5f84025'
+               username={props.user.username}
+               secret={props.user.secret}
+               style={{height:'100%'}}
+               />
+        </div>
+    )
+}
+
+export default ChatsPage;
